a594adbe18365e243296d6176a20297caae1fde8
mn-bots <mohammedmusammiln@gmail.com>
2024-07-19 22:46:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -77,7 +77,7 @@ TUTORIAL = environ.get('TUTORIAL', 'https://t.me/mnbots_support')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mnbots_support') # Support Chat Link Without https:// or @
 
 # True Or False
-AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
+AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', False))
 PM_SEARCH = bool(environ.get('PM_SEARCH', True))
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
